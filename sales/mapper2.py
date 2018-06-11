fedf9c3510ac11b1ed9389196ffadc29f71bfee9
Niucoder <znbjtu@126.com>
2018-06-11 11:30:02 +0800

    Add files via upload

---
+++ sales/mapper2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+import sys
+for line in sys.stdin:
+    line = line.strip()
+    data = line.split('\t')
+    date,time,store,category,cost,method = data
+    print('{0}\t{1}'.format(store,cost))
