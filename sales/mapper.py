bb2181a519136921e0343037517f68609665dac8
Niucoder <znbjtu@126.com>
2018-06-11 11:29:41 +0800

    Update mapper.py

--- sales/mapper.py
+++ sales/mapper.py
@@ -4,5 +4,4 @@ for line in sys.stdin:
     line = line.strip()
     data = line.split('\t')
     date,time,store,category,cost,method = data
-   # print('{0}\t{1}'.format(category,cost))
-    print('{0}\t{1}'.format(store,cost))
+    print('{0}\t{1}'.format(category,cost))
